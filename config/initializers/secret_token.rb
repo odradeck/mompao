ef32e793605bad2ce9acf9e53b0c6e7181e94fa1
odradeck <odradeck@gmail.com>
2013-07-29 08:08:32 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mompao::Application.config.secret_token = '8cbc0e6722659c92d44d532ebc0c2baf04e661b9f5f21066a8eddc5408c91625699a30bd18ab16d1c0d19355061969a23cdabfabd588ff684a1d3d86c964816a'
